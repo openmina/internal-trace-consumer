be4fa47652da9c70b56c64fddd1bec529796edae
Zura Benashvili <zura.benashvili@gmail.com>
2023-03-10 17:04:05 +0400

    t

--- src/internal_trace_consumer.ml
+++ src/internal_trace_consumer.ml
@@ -1,3 +1,4 @@
+open Str
 open Core
 open Async
 module Block_tracing = Block_tracing
@@ -63,22 +64,29 @@ let process_line ~rotated line =
     eprintf "[ERROR] could not parse line: %s\n%!" line ;
     true
 
-let rec process_reader ~rotated reader =
-  let%bind next_line =
-    Reader.really_read_line ~wait_time:(Time.Span.of_sec 0.5) reader
+let process_readers readers =
+  let%bind next_lines = readers
+  |> Array.map ~f:(fun r -> Reader.really_read_line r ~wait_time:(Time.Span.of_sec 0.5))
+  |> Deferred.Array.all
   in
-  match next_line with
-  | Some line ->
-      if process_line ~rotated line then process_reader ~rotated reader
-      else Deferred.unit
-  | None ->
-      Deferred.unit
 
-let process_file filename =
+      Deferred.unit
+  (* match next_line with *)
+  (* | Some line -> *)
+  (*     if process_line ~rotated line then process_reader ~rotated reader *)
+  (*     else Deferred.unit *)
+  (* | None -> *)
+  (*     Deferred.unit *)
+
+let process_files filenames =
   let rec loop rotated =
     let%bind result =
       try_with (fun () ->
-          Reader.with_file filename ~f:(process_reader ~rotated) )
+          let readers = Array.map ~f:(fun filename ->
+            Reader.open_file filename) filenames in
+          let%bind readers = Deferred.Array.all readers in
+          process_readers readers
+      )
     in
     match result with
     | Ok () ->
@@ -86,15 +94,38 @@ let process_file filename =
         let%bind () = Clock.after (Time.Span.of_sec 2.0) in
         loop true
     | Error exn ->
-        eprintf
-          "File '%s' could not be opened, retrying after 5 seconds. Reason:\n\
-           %s\n\
-           %!"
-          filename (Exn.to_string exn) ;
+        (* eprintf *)
+        (*   "File '%s' could not be opened, retrying after 5 seconds. Reason:\n\ *)
+        (*    %s\n\ *)
+        (*    %!" *)
+        (*   filename (Exn.to_string exn) ; *)
         let%bind () = Clock.after (Time.Span.of_sec 5.0) in
         loop rotated
   in
   loop false
+
+let get_number_suffix str =
+  let re = regexp "[0-9]*$" in
+  if string_match re str 0 then
+    Some (int_of_string (matched_string str))
+  else
+    None
+
+let str_starts_with sub str =
+  let re = regexp ("^" ^ sub) in
+  string_match re str 0
+
+(* Oldest files to start processing from. *)
+let trace_input_files dir_path =
+  let%map files =  Sys.readdir dir_path in
+  Array.sort files ~compare:(fun a b ->
+   let a = Option.value (get_number_suffix a) ~default:0 in
+   let b = Option.value (get_number_suffix b) ~default:0 in
+   Int.compare b a) ;
+  let general_traces = Array.find files ~f:(str_starts_with "internal-trace.jsonl") in
+  let prover_traces = Array.find files ~f:(str_starts_with "prover.jsonl") in
+  let l = Array.of_list [general_traces; prover_traces] in
+  Array.filter_map l ~f:(fun v -> v)
 
 let serve =
   Command.async ~summary:"Internal trace processor with GraphQL server"
@@ -102,9 +133,9 @@ let serve =
        flag "--port" ~aliases:[ "port" ]
          (optional_with_default 9080 int)
          ~doc:"Port for GraphQL server to listen on (default 9080)"
-     and filename =
-       flag "--trace-file" ~aliases:[ "trace-file" ] (required string)
-         ~doc:"Path to internal trace file"
+     and dir_path =
+       flag "--trace-dir" ~aliases:[ "trace-dir" ] (required string)
+         ~doc:"Path to the directory containing internal tracing output"
      in
      fun () ->
        let insecure_rest_server = true in
@@ -117,8 +148,12 @@ let serve =
            ~schema:Graphql_server.schema ~server_description:"GraphQL server"
            port
        in
-       printf "Consuming events from file: %s\n%!" filename ;
-       let%bind () = process_file filename in
+       let%bind files = trace_input_files dir_path in
+       let files = Array.map ~f:(fun f -> dir_path ^ "/" ^ f) files in
+       printf "Consuming events from files: \n" ;
+       Array.iter ~f:(fun x -> print_endline x) files ;
+       print_endline "" ;
+       let%bind () = process_files files in
        printf "Done\n%!" ; Deferred.unit )
 
 let commands = [ ("serve", serve) ]
